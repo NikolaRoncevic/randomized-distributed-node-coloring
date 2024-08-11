77c221a073b57678434bde96150bb8368cae57e6
Nikola Roncevic <nikola.roncevic@inviggo.com>
2024-08-11 21:23:37 +0200

    add aditional check of current iteration, this should help resolve potential race conditions

--- src/node.rs
+++ src/node.rs
@@ -6,20 +6,20 @@ pub struct Node {
     id: usize,
     color: usize,
     stopped: bool,
+    iteration: usize,
     neighbors: Vec<Rc<RefCell<Node>>>,
 }
 
 impl Node {
     pub fn new(id: usize) -> Self {
         let mut rng = rand::thread_rng();
-        let color = rng.gen_range(1..=3);  // Randomly pick a color from {1, 2, 3}
-        
-        // If we assume that we can get all required data from another crate, 
-        // we would most likely populate list of neighbours on creation 
+        let color = rng.gen_range(1..=3);
+
         Node {
             id,
             color,
             stopped: false,
+            iteration: 0,
             neighbors: vec![],
         }
     }
@@ -40,59 +40,59 @@ impl Node {
         self.stopped
     }
 
-    pub fn pick_and_send_color(&mut self) { 
+    pub fn get_iteration(&self) -> usize {
+        self.iteration
+    }
+
+    pub fn pick_and_send_color(&mut self) {
         if self.stopped {
             return;
         }
 
         let mut rng = rand::thread_rng();
-        self.color = rng.gen_range(1..=3);  // Pick a new random color
+        self.color = rng.gen_range(1..=3);
+        self.iteration += 1;
 
-        self.send_color()
+        self.send_color();
     }
 
     pub fn send_color(&self) {
-        // here we would inform other nodes that we have updated our color.
-        // Also in case of having some central component that would handle synchronization between nodes 
-        // Notify neighbors of the new color
-        for _neighbor in &self.neighbors {
-            // here we would invoke call to crate that manages sending data to other nodes
-            //let mut neighbor_borrowed = neighbor.borrow_mut();
-            //neighbor_borrowed.receive_color(self.id, self.color);
+        // this should be api call, on other node this would trigger receive color
+    }
+
+    pub fn receive_color(&mut self, sender: Rc<RefCell<Node>>) {
+        let sender_borrowed = sender.borrow();
+        // Ensure the sender's iteration is aligned with the current node's iteration
+        if !self.stopped && sender_borrowed.get_iteration() != self.iteration {
+            return;
+        }
+
+        // Check if all neighbors are either stopped or in the same iteration
+        let all_synced = self.neighbors.iter().all(|neighbor| {
+            let neighbor = neighbor.borrow();
+            neighbor.has_stopped() || neighbor.get_iteration() == self.iteration
+        });
+
+        // If all neighbors are synchronized, process the neighbors
+        if all_synced {
+            self.process_neighbors();
         }
     }
 
-    pub fn receive_color(&mut self, _sender_id: usize, _sender_color: usize) {
-        // Update color directly without nested mutable borrows
-        // This code will not be used, but demostrantes how we cound hypotetically react on receiving color
-        // in this implementation we could have issues with race conditions
-
-        // if let Some(neighbor) = self.neighbors.iter().find(|n| n.borrow().get_id() == sender_id) {
-        //     neighbor.borrow_mut().color = sender_color;
-        // }
-    
-        // // Now process neighbors after receiving color
-        // self.process_neighbors();
-    }
-
     pub fn process_neighbors(&mut self) {
-        // Processing of neigbours should wait until
-        // all nodes that have not stopped pick_and_send_collor, we can discuss how we would approach this 
         if self.stopped {
             return;
         }
 
-        // Check if any neighbor has the same color
+        // Check for conflicts with any neighbor
         let conflict = self.neighbors.iter().any(|neighbor| neighbor.borrow().get_color() == self.color);
 
         if !conflict {
-            // potentially here we should notify that this node is done, and that it wont be processed in future
-            // this would play a part in determingin when not finished nodes can start picking colors again
             self.stopped = true;
         }
     }
 
     pub fn print_state(&self) {
-        println!("Node {}: color = {}, stopped = {}", self.id, self.color, self.stopped);
+        println!("Node {}: color = {}, stopped = {}, iteration = {}", self.id, self.color, self.stopped, self.iteration);
     }
 }
